aae6257bf2cf6337d818a9309fb698c2fa64515b
JUAN CHAVEZ <juanchavezsaldana1@gmail.com>
2022-06-09 20:59:06 -0500

    Definiendo las variables principales

--- src/la_distancia_mas_corta/La_distancia_mas_corta.java
+++ src/la_distancia_mas_corta/La_distancia_mas_corta.java
@@ -1,6 +1,8 @@
 
 package la_distancia_mas_corta;
 
+import javax.swing.JOptionPane;
+
 
 
 public class La_distancia_mas_corta {
@@ -8,7 +10,13 @@ public class La_distancia_mas_corta {
     public static void main(String[] args) {
         
     //EMPEZANDO EL PROGRAMA :)
-      
+     
+    //Definiendo Variable
+    int num_nodos;
+    
+    //Definiendo la matriz de distancias en funcion del numero de nodos
+    String M_D[][];
+    
     
     
     }
