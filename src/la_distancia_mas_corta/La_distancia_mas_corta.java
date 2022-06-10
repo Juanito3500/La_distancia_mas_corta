2ec1148a7f684ec52561092396a58cafbeeca615
JUAN CHAVEZ <juanchavezsaldana1@gmail.com>
2022-06-09 22:11:40 -0500

    mas avances...

--- src/la_distancia_mas_corta/La_distancia_mas_corta.java
+++ src/la_distancia_mas_corta/La_distancia_mas_corta.java
@@ -16,6 +16,20 @@ public class La_distancia_mas_corta {
     
     //Definiendo la matriz de distancias en funcion del numero de nodos
     String M_D[][];
+    //Definiendo el tamaño de la matriz M_D
+    M_D = new String[num_nodos-1][num_nodos-1];
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -23,17 +37,43 @@ public class La_distancia_mas_corta {
     
     //Suponiendo que M_D esta rellenada
     //Empezando a modificar nuestra matriz de distancias 
-        //Bucles de barrido de la matriz M_D
     
-        for(int fil = 0;fil <= num_nodos-1;fil++){
+        //Definiendo una matriz unidimensional para 
+        //filas disponbles y columnas disponibles
+        int filas_y_columnas_disponibles[];
+        //Inicializando la matriz unidimensional de filas y columnas disponibles
+        filas_y_columnas_disponibles=new int[num_nodos-2];
+        //Definiendo varios contadores
+        int fil_col;
+        
+        //Bucle de barrido de la matriz M_D
+        for(int u = 0;u < num_nodos;u++){
             
-            for(int col = 0; col <= num_nodos-1;col++){
+            fil_col=0;
+            //Obteniendo las filas y columnas disponibles de cada ciclo de u
+            //en la matriz filas_y_columnas_disponibles[]
+            //==================================================
+            for(int k = 0;k <num_nodos;k++){
+                
+                if(k!=u){
+                    filas_y_columnas_disponibles[fil_col]=k;
+                    fil_col++;
+                } 
+            }
+            //==================================================
+            
+            for(int index_f=0;index_f<num_nodos;index_f++){
                 
                 
+                
+                for(int index_c=0;index_c<num_nodos;index_c++){
+                    
+                    
+                    
+                }
                 
             }
+            
         }
-    
-    }
     
 }
